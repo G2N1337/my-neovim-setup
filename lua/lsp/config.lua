9ea527d6f9181a36eb8f1c09ce9010138b1e7bcd
Yan <yan@MacBook-Pro-Yan.local>
2023-04-30 20:32:15 +0700

    test

--- lua/lsp/config.lua
+++ lua/lsp/config.lua
@@ -23,7 +23,7 @@ local codes = {
   },
   redefinition = {
     message = " That variable was defined before",
-    icon = " ",
+    icon = " ijsdiasjd",
     "redefinition",
     "redefined-local",
     "no-duplicate-imports",
